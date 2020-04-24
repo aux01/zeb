6970f734f8983fffcc5b12f816d48148ebd8de79
Auxrelius I <aux01@aux.life>
2020-04-24 15:40:09 -0500

    Fill width with spaces and return to line start
    The previous approach of counting line characters and filling spaces
    at the end to meet the requested width is complicated because not
    all characters occupy one character cell when displayed. For
    instance:
    
     - Tab characters can be various widths based on tabs(1) settings.
     - Zero-width and non-visible characters like escape codes.
     - Wide characters that take up multiple character cells.
    
    It may be possible to work through all this to determine visible
    character width but it'd take a while.
    
    The downside to the new approach is that the output size is greatly
    increased and the \r and space characters may cause problems when
    piping output to filter commands or copy/pasting.

--- altpp.c
+++ altpp.c
@@ -4,26 +4,39 @@
 #define LINEMAX (1024 * 4)
 
 int main(void) {
+        // width of each table row in character cells
         int width = 80;
 
+        // alternating color sequences
         char *colors[] = { "\e[102m", "\e[103m" };
         int colorsz = strlen(colors[0]);
+
+        // reset
         char *coloroff = "\e[0m";
         int coloroffsz = strlen(coloroff);
 
-        char space[LINEMAX];
-        for (int i = 0; i < LINEMAX; i++)
-                space[i] = ' ';
+        // buffer of space characters
+        char spaces[LINEMAX];
+        memset(spaces, ' ', LINEMAX - 1);
 
+        // current line number
+        int lineno = 0;
+
+        // getline accounting
         char *line = NULL;
         size_t linecap = 0;
         ssize_t linelen;
 
-        int lineno = 0;
         while ((linelen = getline(&line, &linecap, stdin)) > 0) {
+                // write color sequence followed by N space chars, followed
+                // by \r (move to beginning of line)...
                 fwrite(colors[lineno++ % 2], colorsz, 1, stdout);
+                fwrite(spaces, width, 1, stdout);
+                fwrite("\r", 1, 1, stdout);
+
+                // write actual line chars, followed by the color reset
+                // sequence, followed by the newline.
                 fwrite(line, linelen - 1, 1, stdout);
-                fwrite(space, width - linelen, 1, stdout);
                 fwrite(coloroff, coloroffsz, 1, stdout);
                 fwrite("\n", 1, 1, stdout);
         }
